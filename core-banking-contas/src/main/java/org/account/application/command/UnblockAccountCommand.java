3671b1ae3c342474116ad9cfafa12d46012bab78
wladysson <wladysonaraujo991@gmail.com>
2026-06-09 02:20:00 -0300

    feat(account-command): add UnblockAccountCommand, Prepare CQRS write-side for account status restoration, Encapsulate AccountId as command payload

---
+++ core-banking-contas/src/main/java/org/account/application/command/UnblockAccountCommand.java
@@ -0,0 +1,16 @@
+package com.bank.account.application.command;
+
+import com.bank.account.domain.model.AccountId;
+
+public class UnblockAccountCommand {
+
+    private final AccountId accountId;
+
+    public UnblockAccountCommand(AccountId accountId) {
+        this.accountId = accountId; // conta que será desbloqueada
+    }
+
+    public AccountId getAccountId() {
+        return accountId; // retorna identificador da conta
+    }
+}
