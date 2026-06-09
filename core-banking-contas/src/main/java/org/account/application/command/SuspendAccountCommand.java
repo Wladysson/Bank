bb122516350ae7d5ab81b0b05c0df6893faa7788
wladysson <wladysonaraujo991@gmail.com>
2026-06-09 01:05:00 -0300

    feat(account-command): add SuspendAccountCommand, Introduce command for account suspension workflow, Support CQRS write-side lifecycle control

--- core-banking-contas/src/main/java/org/account/application/command/SuspendAccountCommand.java
+++ core-banking-contas/src/main/java/org/account/application/command/SuspendAccountCommand.java
@@ -1,2 +1,23 @@
+package com.bank.account.application.command;
 
+import com.bank.account.account.domain.model.AccountId;
+import com.bank.account.account.domain.model.AccountStatusReason;
 
+public class SuspendAccountCommand {
+
+    private final AccountId accountId;
+    private final AccountStatusReason reason;
+
+    public SuspendAccountCommand(AccountId accountId, AccountStatusReason reason) {
+        this.accountId = accountId; // conta que será suspensa
+        this.reason = reason; // motivo da suspensão
+    }
+
+    public AccountId getAccountId() {
+        return accountId; // retorna conta alvo
+    }
+
+    public AccountStatusReason getReason() {
+        return reason; // retorna motivo da suspensão
+    }
+}
