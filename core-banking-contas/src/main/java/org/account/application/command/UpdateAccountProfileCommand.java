1de8862b73c3b62e500d3038f41b4b3b80500383
wladysson <wladysonaraujo991@gmail.com>
2026-06-09 03:10:00 -0300

    feat(account-command): add UpdateAccountProfileCommand, Prepare maintenance and administrative account workflows

---
+++ core-banking-contas/src/main/java/org/account/application/command/UpdateAccountProfileCommand.java
@@ -0,0 +1,34 @@
+package com.bank.account.application.command;
+
+import com.bank.account.domain.model.AccountId;
+import com.bank.account.domain.model.AccountMetadata;
+import com.bank.account.domain.model.AccountSettings;
+
+public class UpdateAccountProfileCommand {
+
+    private final AccountId accountId;
+    private final AccountSettings settings;
+    private final AccountMetadata metadata;
+
+    public UpdateAccountProfileCommand(
+            AccountId accountId,
+            AccountSettings settings,
+            AccountMetadata metadata
+    ) {
+        this.accountId = accountId; // conta que será atualizada
+        this.settings = settings; // configurações atualizadas
+        this.metadata = metadata; // metadados atualizados
+    }
+
+    public AccountId getAccountId() {
+        return accountId; // retorna conta alvo
+    }
+
+    public AccountSettings getSettings() {
+        return settings; // retorna novas configurações
+    }
+
+    public AccountMetadata getMetadata() {
+        return metadata; // retorna novos metadados
+    }
+}
