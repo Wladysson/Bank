7cfcd0c9cfb50a36f706b4048a254eb129f073c4
wladysson <wladysonaraujo991@gmail.com>
2026-06-12 18:05:00 -0300

    refactor(usecase): Refactoring the package import process with correction of communication between classes.

--- core-banking-contas/src/main/java/org/account/application/usecase/OpenJointAccountUseCase.java
+++ core-banking-contas/src/main/java/org/account/application/usecase/OpenJointAccountUseCase.java
@@ -1,8 +1,8 @@
 package com.bank.account.account.application.usecase;
 
-import com.bank.account.account.application.command.OpenAccountCommand;
-import com.bank.account.account.application.service.AccountOpeningService;
-import com.bank.account.account.domain.model.Account;
+import com.bank.account.application.command.OpenAccountCommand;
+import com.bank.account.application.service.AccountOpeningService;
+import com.bank.account.domain.model.Account;
 
 public class OpenJointAccountUseCase {
 
@@ -14,6 +14,6 @@ public class OpenJointAccountUseCase {
 
     public Account execute(OpenAccountCommand command) {
 
-        return accountOpeningService.openJointAccount(command); // executa abertura de conta conjunta
+        return accountOpeningService.open(command); // executa abertura de conta conjunta
     }
 }
