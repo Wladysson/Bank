e2b1a7490db9cbecf21a00292017574a0dd40a01
wladysson <wladysonaraujo991@gmail.com>
2026-06-12 11:20:00 -0300

    feat(account-usecase): implement individual account opening, Creates OpenRetailAccountUseCase, Injects AccountOpeningService to execute the operation

---
+++ core-banking-contas/src/main/java/org/account/application/usecase/OpenRetailAccountUseCase.java
@@ -0,0 +1,20 @@
+package com.bank.account.account.application.usecase;
+
+import com.bank.account.application.command.OpenAccountCommand;
+import com.bank.account.application.service.AccountOpeningService;
+import com.bank.account.domain.model.Account;
+
+
+public class OpenRetailAccountUseCase {
+
+    private final AccountOpeningService accountOpeningService;
+
+    public OpenRetailAccountUseCase(AccountOpeningService accountOpeningService) {
+        this.accountOpeningService = accountOpeningService; // injeta serviço de abertura
+    }
+
+    public Account execute(OpenAccountCommand command) {
+
+        return accountOpeningService.openRetailAccount(command); // executa abertura de conta PF
+    }
+}
